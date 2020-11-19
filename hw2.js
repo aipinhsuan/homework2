45e72dd5133766ca6e5d34906ecbf6433e671e29
JackyLin <linsinwei731@gmail.com>
2020-11-19 13:22:11 +0800

    更新

--- hw2.js
+++ hw2.js
@@ -2,10 +2,38 @@ var Calculator = /** @class */ (function () {
     function Calculator() {
         this.operand_1 = "";
         this.operand_2 = "";
+        this.operator = "";
         this.displayString = "";
         this.isFirst = true;
     }
+    Calculator.prototype.Clear = function () {
+        this.operand_1 = "";
+        this.operand_2 = "";
+        this.operator = "";
+        this.displayString = "0";
+        this.isFirst = true;
+        this.Display();
+    };
+    Calculator.prototype.Percent = function () {
+        this.displayString = (+this.operand_1 / 100.0) + "";
+        this.Display();
+    };
+    Calculator.prototype.Reciprocal = function () {
+        this.displayString = (1 / +this.operand_1) + "";
+        this.Display();
+    };
+    Calculator.prototype.Power = function () {
+        this.displayString = Math.pow(+this.operand_1, 2) + "";
+        this.Display();
+    };
+    Calculator.prototype.Square = function () {
+        this.displayString = Math.pow(+this.operand_1, 0.5) + "";
+        this.Display();
+    };
     Calculator.prototype.NumberButton = function (num) {
+        if (this.displayString === "0") {
+            this.displayString = "";
+        }
         if (this.isFirst) {
             this.operand_1 += num;
             this.displayString += num;
@@ -16,15 +44,31 @@ var Calculator = /** @class */ (function () {
         }
         this.Display();
     };
-    Calculator.prototype.Add = function () {
+    Calculator.prototype.SetOperator = function (op) {
         this.isFirst = false;
-        this.displayString += '+';
+        this.displayString += op;
+        this.operator = op;
         this.Display();
     };
     Calculator.prototype.Equal = function () {
+        this.isFirst = true;
         var operand_1 = +this.operand_1;
         var operand_2 = +this.operand_2;
-        var result = operand_1 + operand_2;
+        var result = 0;
+        switch (this.operator) {
+            case "+":
+                result = operand_1 + operand_2;
+                break;
+            case "-":
+                result = operand_1 - operand_2;
+                break;
+            case "*":
+                result = operand_1 * operand_2;
+                break;
+            case "/":
+                result = operand_1 / operand_2;
+                break;
+        }
         this.displayString = result + "";
         this.Display();
     };
--- hw2.ts
+++ hw2.ts
@@ -2,6 +2,7 @@ class Calculator {
 
     private operand_1:string = "";
     private operand_2:string = "";
+    private operator:string = "";
     private displayString:string = "";
     private isFirst:boolean;
 
@@ -9,7 +10,40 @@ class Calculator {
         this.isFirst = true;
     }
 
+    public Clear(){
+        this.operand_1 = "";
+        this.operand_2 = "";
+        this.operator = "";
+        this.displayString = "0";
+        this.isFirst = true;
+        this.Display();
+    }
+
+    public Percent(){
+        this.displayString = (+this.operand_1 / 100.0) + "";
+        this.Display();
+    }
+
+    public Reciprocal(){
+        this.displayString = (1/+this.operand_1) + "";
+        this.Display();
+    }
+
+    public Power(){
+        this.displayString = Math.pow(+this.operand_1,2)+ "";
+        this.Display();
+    }
+
+    public Square(){
+        this.displayString =  Math.pow(+this.operand_1,0.5) + "";
+        this.Display();
+    }   
+
     public NumberButton(num:string){
+        if (this.displayString === "0"){
+            this.displayString = "";
+        }        
+
         if (this.isFirst){           
             this.operand_1 += num; 
             this.displayString += num;          
@@ -21,16 +55,33 @@ class Calculator {
         this.Display();
     }
 
-    public Add(){
+    public SetOperator(op:string){
         this.isFirst = false;
-        this.displayString += '+';  
+        this.displayString += op;  
+        this.operator = op;
         this.Display();
-    }
+    } 
 
     public Equal(){
-        var operand_1:number = +this.operand_1;
-        var operand_2:number = +this.operand_2;
-        var result = operand_1 + operand_2;
+        this.isFirst = true;
+        var operand_1:number = + this.operand_1;
+        var operand_2:number = + this.operand_2;
+        var result:number = 0;
+        switch(this.operator){
+            case "+":
+                result = operand_1 + operand_2;
+                break;
+            case "-":
+                result = operand_1 - operand_2;
+                break;
+            case "*":
+                result = operand_1 * operand_2;
+                break;
+            case "/":
+                result = operand_1 / operand_2;
+                break;
+        }
+       
         this.displayString = result + "";
         this.Display();
     }
